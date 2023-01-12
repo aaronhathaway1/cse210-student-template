192e8626d9b9aa7f332ce947e5e479e0c7d0c25b
Aaron Hathaway <aaronhathaway1@hotmail.com>
2023-01-11 18:10:29 -0700

    Week 01 Assigments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("What was your grade percentage?");
+    int letterGrade = int.Parse(Console.ReadLine());
+    bool passed = true;
+    char letter = 'A';
+
+    if (letterGrade >= 90)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      letter = 'A';
     }
+    else if (letterGrade >= 80)
+    {
+      letter = 'B';
+    }
+    else if (letterGrade >= 70)
+    {
+      letter = 'C';
+    }
+    else if (letterGrade >= 60)
+    {
+      letter = 'D';
+      passed = false;
+    }
+    else
+    {
+      letter = 'F';
+    }
+
+    Console.WriteLine($"You got a(n) {letter}.");
+
+    if (passed)
+    {
+      Console.WriteLine("You passed. Congratulations!");
+    }
+    else
+    {
+      Console.WriteLine("You failed. You can always try again next semester!");
+    }
+
+
+
+
+
+  }
 }
