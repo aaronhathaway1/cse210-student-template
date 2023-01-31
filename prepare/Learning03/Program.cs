540d75341b04ddfdcc7686df618c525df0d475f1
aaronhathaway1 <aaronhathaway1.com>
2023-01-31 16:06:57 -0700

    Completed Assignment 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
-class Program
+namespace Fractional
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Learning03 World!");
+
+            Fraction fraction1 = new Fraction();
+            Fraction fraction2 = new Fraction(5);
+            Fraction fraction3 = new Fraction(1, 3);
+
+            // fraction1.Display();
+            Console.WriteLine(fraction1.GetFractionString());
+            Console.WriteLine(fraction1.GetDecimalValue());
+
+            // fraction2.Display();
+            Console.WriteLine(fraction2.GetFractionString());
+            Console.WriteLine(fraction2.GetDecimalValue());
+
+            // fraction3.Display();
+            Console.WriteLine(fraction3.GetFractionString());
+            Console.WriteLine(fraction3.GetDecimalValue());
+        }
+
+
     }
 }
