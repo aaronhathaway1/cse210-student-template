cf036fd7618bfa3ed77e583d7af8f14c2a2e2723
Aaron Hathaway <aaronhathaway1@hotmail.com>
2023-01-31 17:21:27 -0700

    Added comments to main

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,12 +2,18 @@ using System;
 
 namespace Journaling
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Journal journal = new Journal();
-            journal.DisplayMenu();
-        }
+      Journal journal = new Journal();
+      journal.DisplayMenu();
+
+      // There is quite a bit of error check
+      // I null check if entries has been instatiated
+      // User input is also checked, although not perfectly
+      // There are 30 prompts that are randomly picked fron
+      // Or the user can chose no prompt.
     }
+  }
 }
